fbd0579c3fbe7f270e204df66a3e5bb3313a3bca
rozpan <rozpan@gmail.com>
2020-08-15 17:36:28 +0300

    corrected task 1 and finished task 2

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -4,26 +4,24 @@
 	2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
-let algebra, geography, physics, averageRating;
-a = algebra;
-g = geography;
-p = physics;
 
 function averageMark(a, g, p) {
-	averageRating = (a + g + p) / 3;
-	return (averageRating); 
+    let algebra, geography, physics, averageRating; 
+	algebra = a;
+	geography = g;
+	physics = p;
+	averageRating = (algebra + geography + physics) / 3;
+	return(averageRating); 
 }
+
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+    let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
+	return(message);
 }
 
+
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+    return
 }
