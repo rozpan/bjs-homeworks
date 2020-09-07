3873b3b895f70c22d75a01e1b68c75869ad0ff0e
rozpan <rozpan@gmail.com>
2020-09-07 16:38:59 +0300

    finished two tasks

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,30 @@
-//String.prototype.isPalindrome - для задачи №1
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function() { 
+    let str = this.replace(/\s/g, '').toLowerCase();
+    let j = str.length;
+
+    for (let i = 0; i < str.length; i++) {
+        j--;
+        if (str[i] == str[j]) {
+            if (i == (str.length - 1) && str[i] == str[j]) {
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    }
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+
+function getAverageMark(marks) {
+    let average; 
+    let sum = 0;
+    for (let value of marks) {
+        sum += value;
+    }
+    average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
