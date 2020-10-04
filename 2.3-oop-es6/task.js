17ee07b74c05c115303ef0e1a1c4e1ed0c3947da
rozpan <rozpan@gmail.com>
2020-10-04 09:20:56 +0300

    finished task two

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -88,8 +88,9 @@ class Library {
     giveBookByName(bookName) {
         for (let i = 0; i < this.books.length; i++) {
                 if (bookName == this.books[i].name) {
-                    return(this.books.splice(i, 1));
-                
+                    let result = this.books[i];
+                    this.books.splice(i, 1);
+                    return result;
                 }
                 else if (i == this.books.length - 1) {
                     return null;
@@ -99,14 +100,14 @@ class Library {
 }
 
 const library = new Library("Библиотека имени Ленина");
-
 library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
 library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
 library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
 library.addBook(new Magazine("Мурзилка", 1924, 60));
 
 console.log(library.findBookBy("name", "Властелин колец")); 
-console.log(library.findBookBy("releaseDate", 1895).name); 
+console.log(library.findBookBy("releaseDate", 1924).name); 
 
-console.log("Количество книг до выдачи: " + library.books.length); 
+console.log("Количество книг до выдачи: " + library.books.length);
+library.giveBookByName("Машина времени");
 console.log("Количество книг после выдачи: " + library.books.length);
