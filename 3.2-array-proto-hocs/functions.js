f8d793342fdb815fa17c82b69daf024ce122a000
rozpan <rozpan@gmail.com>
2021-01-28 16:53:10 +0200

    second task

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -1,4 +1,5 @@
 //задание_1
+/*
 const weapons = [
     new Knife(), 
     new Staff(), 
@@ -48,3 +49,59 @@ function getTotalDamage() {
 	return weapons.reduce((sum, n) => sum + n.attack, 0);
 }
 getTotalDamage();
+*/
+
+
+
+
+
+//задание_2
+function compareArrays (a, b) {
+	function f(n, i) {
+		return n === b[i];
+	}
+	if ((a.length === b.length) && (a.every(f))) {
+	 	return true;
+	}
+	else {
+		return false;
+	}
+}
+function sleep(milliseconds) {
+	let e = new Date().getTime() + milliseconds;
+	while (new Date().getTime() <= e) {}
+}
+function sum(...args) {
+	sleep(100); 
+	/* вычисления суммы пока не нужны...
+	return args.reduce((sum, arg) => {
+	  return sum + arg;
+	}, 0);
+	*/
+	return [...args]; 
+  }
+
+function memorize(fn, limit) {
+	const memory = [
+		{
+		  args: [3, 4],
+		  result: 7
+		},
+		{
+		args: [1, 3],
+		result: 4
+		} 
+	];
+
+	return function bar (a, b) { 
+		if (memory.find(El => compareArrays(El.args, fn(a, b)))) {
+			return (memory.find(El => compareArrays(El.args, fn(a, b)))).result;
+		}
+		else {
+			memory.push({args: fn(a, b)});
+			return memory;
+		}
+	}
+}
+const mSum = memorize(sum, 5);
+console.log(mSum(1, 3)); 
